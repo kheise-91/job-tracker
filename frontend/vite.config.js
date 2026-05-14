7901f35dfee84c11c0762edcd6da79f5bba1e0ed
Kevin Heise <kevin.heise.91@gmail.com>
2026-05-14 01:52:28 -0500

    Removing vite-pwa (setting up manually instead) and adding 512x512 image.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,33 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico'],
-      manifest: {
-        name: 'Job Tracker',
-        short_name: 'JobTracker',
-        description: 'Track your job applications',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
